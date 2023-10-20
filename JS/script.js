4bcb187fe4bb4e73d7d0c0ae4f76c5bf0406a8f4
HarrisonCBerger <100549873+HarrisonCBerger@users.noreply.github.com>
2023-10-19 17:11:36 -0700

    Made my rocket!

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,13 @@
+// chaning class name
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+
+    if (currentClassName == 'cool') {
+        document.getElementById('cool').className = 'cool red';
+    } else {
+        document.getElementById('cool').className = 'cool';
+    }
+
 var sayMyName = function (name) {
     alert('My name is: ' +name);
 }
